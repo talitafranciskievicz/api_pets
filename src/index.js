363ffdb093fd960f4eb763353833759b5e7be49e
Talita <talitafranciskievicz@gmail.com>
2026-09-25 18:15:50 -0300

    Criação do servidor express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello World!!");
+import express from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Definir Rotas
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+  console.log(`O servidor está rodando na porta ${porta}`);
+});
